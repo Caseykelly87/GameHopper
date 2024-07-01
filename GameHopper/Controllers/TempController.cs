3ac730c63c2165ab80f2f42ee162ee41972a504a
jacxd64 <70988180+jacxd64@users.noreply.github.com>
2024-07-01 15:05:08 -0500

    feature navbar rudimentery version w/ views - JG

--- GameHopper/Controllers/TempController.cs
+++ GameHopper/Controllers/TempController.cs
@@ -15,42 +15,36 @@ public class TempController : Controller
     [HttpGet]
     public IActionResult Profile()
     {
-        var html = "<h1>Temporary profile page<h1>";
-        return Content(html, "text/html");
+        return View();
     }
 
     [HttpGet]
     public IActionResult Games()
     {
-        var html = "<h1>Temporary games page<h1>";
-        return Content(html, "text/html");
+        return View();
     }
 
     [HttpGet]
     public IActionResult Featured()
     {
-        var html = "<h1>Temporary Featured page<h1>";
-        return Content(html, "text/html");
+        return View();
     }
 
     [HttpGet]
     public IActionResult FAQs()
     {
-        var html = "<h1>Temporary FAQs page<h1>";
-        return Content(html, "text/html");
+        return View();
     }
 
     [HttpGet]
     public IActionResult Contact()
     {
-        var html = "<h1>Temporary Contact page<h1>";
-        return Content(html, "text/html");
+        return View();
     }
 
     [HttpGet]
     public IActionResult About()
     {
-        var html = "<h1>Temporary About page<h1>";
-        return Content(html, "text/html");
+        return View();
     }
 }
