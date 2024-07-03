7290474b58c04d9856ad3c9777983c6a974a4a7c
dillonwhitener <djwhitener@gmail.com>
2024-07-03 10:30:50 -0500

    Fixed CategoryController name, now causing issues in the controller

--- GameHopper/Controllers/CategoryController.cs
+++ GameHopper/Controllers/CategoryController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using GameHopper.Models;
-using GameHopper.Data;
 
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
