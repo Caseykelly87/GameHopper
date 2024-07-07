fef95e16ef308a55ed5c3d6a7a62b6f863479753
dillon.whitener <djwhitener@gmail.com>
2024-07-07 18:24:49 -0500

    Uploading newest version

--- GameHopper/Controllers/GameController.cs
+++ GameHopper/Controllers/GameController.cs
@@ -11,4 +11,36 @@ public class GameController : Controller {
     {
         return View();
     }
+    public IActionResult Delete()
+        {
+            ViewBag.games = DbContext.Games.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] gameIds)
+        {
+            string answer;
+            Console.WriteLine("Are you sure you want to delete your account?"); 
+            answer = Console.ReadLine();
+
+            if (answer.ToLower().Equals("yes") || answer.ToLower().Equals("y") )
+            {
+        
+        foreach (int gameId in gameIds)
+            {
+            Game theGame = context.Games.Find(gameIds);
+            context.Games.Remove(theGame);
+            }
+            
+            context.SaveChanges();
+
+            return View("/Home");
+            }
+
+            else{
+                return View("/Game");
+            }
+        }
 }
--- GameHopper/Controllers/User Controller.cs
+++ GameHopper/Controllers/User Controller.cs
@@ -1,46 +1,24 @@
 using System.Security.Policy;
+using GameHopper;
 using GameHopper.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using GameHopper.Data;
 
 
 public class GameMaster : User
 {
+    private GameDbContext context;
+
+        public GameMaster(GameDbContext dbContext)
+        {
+            context = dbContext;
+        }
     public ICollection<CreatedGames> CreatedGames { get; set; }
     
     public GameMaster(string name, int Id) : base(name, Id)
     {
     }
-    public IActionResult Delete()
-        {
-            ViewBag.users = DbContext.Users.ToList();
-
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Delete(int[] userId)
-        {
-            string answer;
-            Console.WriteLine("Are you sure you want to delete your account?"); 
-            answer = Console.ReadLine();
-
-            if (answer.ToLower().Equals("yes") || answer.ToLower().Equals("y") )
-            {
-        
-            User theUser = context.Users.Find(userId);
-            context.Users.Remove(theUser);
-            
-            context.SaveChanges();
-
-            return View("/Home");
-            }
-
-            else{
-                return View("/User");
-            }
-        }
+    
 }
 
 public class CreatedGames
