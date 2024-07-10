eda2c45e2cb9359d56d8a7e8dda39eb758eef4a1
meglizcode <meglizjudd@gmail.com>
2024-07-10 18:29:08 -0500

    blog entry bug stomping

--- GameHopper/Models/BlogEntry.cs
+++ GameHopper/Models/BlogEntry.cs
@@ -1,10 +1,24 @@
-namespace GameHopper.Models
-{
-    public class BlogEntry
-    {
-        public Guid Id { get; set; }
-        public string Content {get; set;}
+namespace GameHopper.Models;
+using GameHopper.Models;
+using Microsoft.Identity.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-        
+public class BlogEntry
+{
+    public Guid Id { get; set; }
+
+    public int UserId { get; set; }
+    public string Content {get; set;}
+
+    public BlogEntry()
+    {
+    }
+
+    public BlogEntry(Guid id, string content, int userid)
+    {
+        Id = id;
+        Content = content;
+        UserId = userid;
     }
 }
