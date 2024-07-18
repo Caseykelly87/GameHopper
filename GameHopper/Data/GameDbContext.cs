9a4ae57989abbb9dc00578e2e4548c6312f08716
Casey Kelly <Caseykelly87@gmail.com>
2024-07-18 10:41:11 -0500

    BLOBworking

--- GameHopper/Data/GameDbContext.cs
+++ GameHopper/Data/GameDbContext.cs
@@ -61,59 +61,6 @@ public class GameDbContext : IdentityDbContext<User, IdentityRole, string>
         
         }
     }
-            // modelBuilder.Entity<Game>()
-            //     .HasOne(g => g.Category)
-            //     .WithMany(c => c.Games)
-            //     .HasForeignKey(g => g.CategoryId);
-
-            // modelBuilder.Entity<Game>()
-            //     .HasOne(g => g.GameMaster)
-            //     .WithMany(gm => gm.CreatedGames)
-            //     .HasForeignKey(g => g.GameMasterId);
-
-            // modelBuilder.Entity<Game>()
-            //     .HasMany(g => g.Tags)
-            //     .WithMany(t => t.Games)
-            //     .UsingEntity<Dictionary<string, object>>(
-            //         "GameTags",
-            //         j => j.HasOne<Tag>().WithMany().HasForeignKey("TagId"),
-            //         j => j.HasOne<Game>().WithMany().HasForeignKey("GameId"));
-
-            // modelBuilder.Entity<Game>()
-            //     .HasMany(g => g.GamePlayers)
-            //     .WithMany(p => p.CurrentGames)
-            //     .UsingEntity<Dictionary<string, object>>(
-            //         "GamePlayers",
-            //         j => j.HasOne<Player>().WithMany().HasForeignKey("PlayerId"),
-            //         j => j.HasOne<Game>().WithMany().HasForeignKey("GameId"));
-
-            // modelBuilder.Entity<User>()
-            //     .HasMany(u => u.CurrentGames)
-            //     .WithMany(g => g.GamePlayers)
-            //     .UsingEntity<Dictionary<string, object>>(
-            //         "UserGames",
-            //         j => j.HasOne<Game>().WithMany().HasForeignKey("GameId"),
-            //         j => j.HasOne<User>().WithMany().HasForeignKey("UserId"));
-
-
-            // modelBuilder.Entity<User>()
-            //     .HasMany(u => u.CurrentGames)
-            //     .WithOne()
-            //     .HasForeignKey(g => g.UserId)
-            //     .OnDelete(DeleteBehavior.Cascade);
-
-            
-            // modelBuilder.Entity<Player>()
-            //     .HasMany(p => p.CurrentGames)
-            //     .WithOne()
-            //     .HasForeignKey(g => g.UserId)
-            //     .OnDelete(DeleteBehavior.Cascade);
-        
-            // modelBuilder.Entity<Player>()
-            //     .HasOne(u => u.Blog)
-            //     .WithOne(b => b.Author)
-            //     .HasForeignKey<BlogEntry>(b => b.AuthorId);
     
-
 
 
