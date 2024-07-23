8e8560581f013a7e94841f8a70f6bcdd5543c363
dillonwhitener <djwhitener@gmail.com>
2024-07-22 22:58:03 -0500

    Finished Categories. Addable, Editable, Deletable

--- GameHopper/Controllers/CategoryController.cs
+++ GameHopper/Controllers/CategoryController.cs
@@ -1,14 +1,11 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using GameHopper.Models;
+using GameHopper.ViewModels;
 
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GameHopper.Controllers
 {
-    public class CategoryController : Controller 
+    public class CategoryController : Controller
     {
         private GameDbContext context;
 
@@ -16,15 +13,118 @@ namespace GameHopper.Controllers
         {
             context = dbContext;
         }
-
-        // GET: /<controller>/
         public IActionResult Index()
         {
             List<Category> categories = context.Categories.ToList();
-            ViewBag.categories = categories;
+            return View(categories);
+        }
+
+
+        //needs to be set for Admin Only
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            
+            {
+        
+                Category newCategory = new Category
+                {
+                    Name = addCategoryViewModel.CategoryName,
+                    Id = addCategoryViewModel.Id
+                };
+
+                bool categories = context.Categories.Any(x => x.Name == newCategory.Name);
+
+
+                    if (categories)
+                    {
+
+                        return Redirect("/Category/Add/");
+                        
+                    }
+
+                    else
+                    {
+
+                    context.Categories.Add(newCategory);
+                    context.SaveChanges();
+                    return Redirect("/Category/");
+                    }
+                }
+                return View("Add", addCategoryViewModel);
+            }
+          
+          public IActionResult Delete()
+        {
+            ViewBag.categories = context.Categories.ToList();
+
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Delete(int[] categoryIds)
+        {
+            foreach (int categoryId in categoryIds)
+            {
+                Category theCategory = context.Categories.Find(categoryId);
+                context.Categories.Remove(theCategory);
+            }
+
+            context.SaveChanges();
+
+            return Redirect("/Category/");
+        }
+
+        public IActionResult Edit()
+        {
+            ViewBag.Categories = context.Categories.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int[] categoryIds, AddCategoryViewModel addCategoryViewModel)
+        {
+            foreach (int categoryId in categoryIds)
+            {
+                Category oldCategory = context.Categories.Find(categoryId);
+
+                Category newCategory = new Category
+                {
+                    Name = addCategoryViewModel.CategoryName,
+                    Id = categoryId
+                };
+                bool categories = context.Categories.Any(x => x.Name == newCategory.Name);
+
+
+                    if (categories)
+                    {
+
+                        return Redirect("/Category/Edit/");
+                        
+                    }
+
+                    else
+                    {
+
+                context.Categories.Remove(oldCategory);
+                context.Categories.Add(newCategory);
+                    }
+                    context.SaveChanges();
+            }
+
+
+            return Redirect("/Category/");
+        }
+            
+        }
         
     }
-}
